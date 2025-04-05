ffa5f93c9c84989aa4d41fc39373069a4d9033ed
xnKE7 <143867953+xnKE7@users.noreply.github.com>
2025-04-04 21:44:32 -0600

    Add files via upload

---
+++ M2A2 - Actividad 2 - Uso de DOM y buenas prácticas/src/Scripts/funciones.js
@@ -0,0 +1,28 @@
+function valores() {
+    let filas = parseInt(document.getElementById("filas").value);
+    let columnas = parseInt(document.getElementById("columnas").value);
+try{
+    if (isNaN(filas) || isNaN(columnas)) {
+        alert("Los valores no son numericos");
+        return;
+    }
+
+    if (filas <= 0 || columnas <= 0) {
+        alert("El valor debe ser mayor que 0");
+        return;
+    }
+
+    crearTabla(filas, columnas);
+   }
+   catch{
+    alert("Error: " + error.message);
+   }
+}
+
+import { crearTabla } from"./funcionesAvanzadas.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("boton").addEventListener("click", leerDatos);
+});
+
+
