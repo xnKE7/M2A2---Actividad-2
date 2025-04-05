51dcd50578c0cc38ab0be83ec862082bc54d25ce
xnKE7 <andres.bustos6391@alumnos.udg.mx>
2025-04-04 20:43:38 -0600

    V3

--- src/Scripts/funciones.js
+++ src/Scripts/funciones.js
@@ -1,8 +1,20 @@
 function valores() {
     let filas = parseInt(document.getElementById("filas").value);
     let columnas = parseInt(document.getElementById("columnas").value);
+
+    if (isNaN(filas) || isNaN(columnas)) {
+        alert("Los valores no son numericos");
+        return;
+    }
+
+    if (filas <= 0 || columnas <= 0) {
+        alert("El valor debe ser mayor que 0");
+        return;
+    }
+
     crearTabla(filas, columnas);
 }
+
 
 function crearTabla(filas, columnas) {
     let codigo = "<table border='1'>";
